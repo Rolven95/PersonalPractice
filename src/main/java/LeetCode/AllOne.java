6477349bed1c40f5f617f9238c9c351a708506bf
RuowenDeepin <chengruowen@vip.qq.com>
2019-06-24 19:13:09 +0800

    [Refactor] TreeMap

--- src/main/java/LeetCode/AllOne.java
+++ src/main/java/LeetCode/AllOne.java
@@ -1,6 +1,10 @@
 package LeetCode;
 
-import java.util.HashMap;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Implement a data structure supporting the following operations:
@@ -19,60 +23,50 @@ import java.util.HashMap;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class AllOne {
-    HashMap<String, Integer> dataMap;
-    int MaxValue;
-    int MaxCount;
+    public static void main(String[] args) {
+        AllOne allOne = new AllOne();
+        allOne.inc("a");
+        allOne.inc("a");
+        String xa2 = allOne.getMaxKey();
+        String ia2 = allOne.getMinKey();
+        allOne.inc("b");
+        String ib1 = allOne.getMaxKey();
+        allOne.inc("b");
+        allOne.dec("a");
+        String xb2 = allOne.getMaxKey();
+        String ia1 = allOne.getMinKey();
+    }
 
-    int MinValue;
-    int MinCount;
 
-    int freshValue;
-    int dataSize;
+    private Comparator<Map.Entry<String, Integer>> dataModelComparator = new Comparator<Map.Entry<String, Integer>>() {
+        @Override
+        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            if (o1.getValue() < o2.getValue())
+                return 1;
+            if (o1.getValue() > o2.getValue())
+                return -1;
+            return 0;
+        }
+    };
+
+    private TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>(dataModelComparator);
 
     /**
      * Initialize your data structure here.
      */
     public AllOne() {
-        dataMap = new HashMap<>();
-        freshValue = 0;
-        dataSize = 0;
-        MinValue = -1;
-        MaxValue = -1;
     }
 
     /**
      * Inserts a new key <Key> with value 1. Or increments an existing key by 1.
      */
     public void inc(String key) {
-        if (dataMap.containsKey(key)) {
-            int originalVal = dataMap.get(key);
-            if (originalVal == 1) {
-                freshValue--;
-            }
-            int newVal = originalVal + 1;
-            if (newVal > MaxValue) {
-                MaxValue = newVal;
-                MaxCount = 1;
-            }else if(newVal == MaxValue){
-                MaxCount ++;
-            }
-
-            dataMap.put(key, newVal);
+        if (treeMap.containsKey(key)) {
+            int newValue = 1 + treeMap.get(key);
+            treeMap.remove(key);
+            treeMap.put(key, newValue);
         } else {
-            dataSize++;
-            freshValue++;
-            dataMap.put(key, 1);
-
-            if(1 > MaxValue){
-                MaxValue = 1;
-                MaxCount = 1;
-            }else if(1 == MaxValue) {
-                MaxCount ++;
-            }
-
-            if(MinValue == -1){
-                MinValue ==
-            }
+            treeMap.put(key, 1);
         }
     }
 
@@ -80,36 +74,37 @@ public class AllOne {
      * Decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
      */
     public void dec(String key) {
-        if(!dataMap.containsKey(key)){
-            return;
+        if (treeMap.containsKey(key)) {
+            int newValue = treeMap.get(key) - 1;
+            if (newValue <= 0) {
+                treeMap.remove(key);
+            } else {
+                treeMap.remove(key);
+                treeMap.put(key, newValue);
+            }
         }
-        int originalVal = dataMap.get(key);
-        if (originalVal == MaxValue){
-            MaxCount --;
-        }
-
-
-        int newVal = originalVal - 1 ;
-        if(newVal == 0){
-            dataMap.remove(key);
-            dataSize --;
-        }
-
-
     }
 
     /**
      * Returns one of the keys with maximal value.
      */
     public String getMaxKey() {
-
+        String firstKey = treeMap.firstKey();
+        if(StringUtils.isNotEmpty(firstKey))
+            return firstKey;
+        else
+            return "";
     }
 
     /**
      * Returns one of the keys with Minimal value.
      */
     public String getMinKey() {
-
+        String lastKey = treeMap.lastKey();
+        if(StringUtils.isNotEmpty(lastKey))
+            return lastKey;
+        else
+            return "";
     }
     /**
      * Your AllOne object will be instantiated and called as such:
