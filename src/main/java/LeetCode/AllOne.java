e5c321add67ef3c334a2795690872499f8200fcc
RuowenD2 <chengruowen@vip.qq.com>
2019-06-26 21:51:40 +0800

    [Refactor] TreeSet + HashMap

--- src/main/java/LeetCode/AllOne.java
+++ src/main/java/LeetCode/AllOne.java
@@ -2,9 +2,8 @@ package LeetCode;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
+import javax.swing.*;
+import java.util.*;
 
 /**
  * Implement a data structure supporting the following operations:
@@ -23,33 +22,53 @@ import java.util.TreeMap;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class AllOne {
-    public static void main(String[] args) {
-        AllOne allOne = new AllOne();
-        allOne.inc("a");
-        allOne.inc("a");
-        String xa2 = allOne.getMaxKey();
-        String ia2 = allOne.getMinKey();
-        allOne.inc("b");
-        String ib1 = allOne.getMaxKey();
-        allOne.inc("b");
-        allOne.dec("a");
-        String xb2 = allOne.getMaxKey();
-        String ia1 = allOne.getMinKey();
-    }
+//    public static void main(String[] args) {
+//        AllOne allOne = new AllOne();
+//        allOne.inc("a");
+//        allOne.inc("a");
+//        String xa2 = allOne.getMaxKey();
+//        String ia2 = allOne.getMinKey();
+//        allOne.inc("b");
+//        String ib1 = allOne.getMaxKey();
+//        allOne.inc("b");
+//        allOne.inc("b");
+//        allOne.inc("b");
+//        allOne.dec("a");
+//        String xb2 = allOne.getMaxKey();
+//        String ia1 = allOne.getMinKey();
+//    }
 
-
-    private Comparator<Map.Entry<String, Integer>> dataModelComparator = new Comparator<Map.Entry<String, Integer>>() {
-        @Override
-        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-            if (o1.getValue() < o2.getValue())
-                return 1;
-            if (o1.getValue() > o2.getValue())
-                return -1;
+    private Comparator<DataModel> DataModelComparator = (o1, o2) -> {
+        if (o1.key.equals(o2.key))
             return 0;
-        }
+        if (o1.getValue() < o2.getValue())
+            return 1;
+        if (o1.getValue() > o2.getValue())
+            return -1;
+        return -1;
     };
 
-    private TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>(dataModelComparator);
+    private class DataModel {
+        int value;
+        String key;
+
+        DataModel(String k, int v) {
+            value = v;
+            key = k;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+    }
+
+    private TreeSet<DataModel> treeSet = new TreeSet<>(DataModelComparator);
+
+    private HashMap<String, Integer> relation = new HashMap<>();
 
     /**
      * Initialize your data structure here.
@@ -61,12 +80,18 @@ public class AllOne {
      * Inserts a new key <Key> with value 1. Or increments an existing key by 1.
      */
     public void inc(String key) {
-        if (treeMap.containsKey(key)) {
-            int newValue = 1 + treeMap.get(key);
-            treeMap.remove(key);
-            treeMap.put(key, newValue);
+        if (relation.containsKey(key)) {
+            DataModel old = new DataModel(key, relation.get(key));
+            relation.remove(key);
+            treeSet.remove(old);
+            int newValue = old.value + 1;
+
+            relation.put(key, newValue);
+            DataModel newModel = new DataModel(key, newValue);
+            treeSet.add(newModel);
         } else {
-            treeMap.put(key, 1);
+            relation.put(key, 1);
+            treeSet.add(new DataModel(key, 1));
         }
     }
 
@@ -74,13 +99,14 @@ public class AllOne {
      * Decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
      */
     public void dec(String key) {
-        if (treeMap.containsKey(key)) {
-            int newValue = treeMap.get(key) - 1;
-            if (newValue <= 0) {
-                treeMap.remove(key);
-            } else {
-                treeMap.remove(key);
-                treeMap.put(key, newValue);
+        if (relation.containsKey(key)) {
+            DataModel old = new DataModel(key, relation.get(key));
+            relation.remove(key);
+            treeSet.remove(old);
+            int newValue = old.value - 1;
+            if (newValue > 0) {
+                relation.put(key, newValue);
+                treeSet.add(new DataModel(key, newValue));
             }
         }
     }
@@ -89,22 +115,20 @@ public class AllOne {
      * Returns one of the keys with maximal value.
      */
     public String getMaxKey() {
-        String firstKey = treeMap.firstKey();
-        if(StringUtils.isNotEmpty(firstKey))
-            return firstKey;
-        else
+        if(treeSet.isEmpty())
             return "";
+        DataModel first = treeSet.first();
+        return first.key;
     }
 
     /**
      * Returns one of the keys with Minimal value.
      */
     public String getMinKey() {
-        String lastKey = treeMap.lastKey();
-        if(StringUtils.isNotEmpty(lastKey))
-            return lastKey;
-        else
+        if(treeSet.isEmpty())
             return "";
+        DataModel first = treeSet.last();
+        return first.key;
     }
     /**
      * Your AllOne object will be instantiated and called as such:
