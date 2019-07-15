5b0a89b4655cfbdb56d2eba13b31f532e205c6fd
Ruowen <chengruowen@vip.qq.com>
2019-07-15 11:17:16 +0800

    [FixBug] All one passed

--- src/main/java/LeetCode/AllOne.java
+++ src/main/java/LeetCode/AllOne.java
@@ -1,8 +1,7 @@
 package LeetCode;
 
-import org.apache.commons.lang3.StringUtils;
+import com.sun.istack.internal.Nullable;
 
-import javax.swing.*;
 import java.util.*;
 
 /**
@@ -22,35 +21,106 @@ import java.util.*;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class AllOne {
-//    public static void main(String[] args) {
-//        AllOne allOne = new AllOne();
-//        allOne.inc("a");
-//        allOne.inc("a");
-//        String xa2 = allOne.getMaxKey();
-//        String ia2 = allOne.getMinKey();
-//        allOne.inc("b");
-//        String ib1 = allOne.getMaxKey();
-//        allOne.inc("b");
-//        allOne.inc("b");
-//        allOne.inc("b");
-//        allOne.dec("a");
+    public static void main(String[] args) {
+
+
+/*
+["AllOne","inc","inc","inc","dec","inc","inc","getMaxKey","dec","dec","dec","getMaxKey"]
+[[],["hello"],["world"],["hello"],["world"],["hello"],["leet"],[],["hello"],["hello"],["hello"],[]]
+*/
+
+
+        AllOne allOne = new AllOne();
+        allOne.inc("hello");
+        allOne.inc("world");
+        allOne.inc("hello");
+
+        allOne.dec("world");
+        allOne.inc("hello");
+        allOne.inc("leet");
+        String ib1 = allOne.getMaxKey(); //
+        String ia2ss = allOne.getMinKey(); //
+
+        allOne.dec("hello");
+        allOne.dec("hello");
+        allOne.dec("hello");
+        String ib1d = allOne.getMaxKey(); //
+        String ia2dss = allOne.getMinKey(); //
+
+
+//        allOne.inc("a"); //1
+//        allOne.inc("a"); //2
+//        String xa2 = allOne.getMaxKey(); //
+//        String ia2 = allOne.getMinKey(); //
+//        allOne.inc("b"); //1
+//        String ib1 = allOne.getMaxKey(); //
+//        String ia2ss = allOne.getMinKey(); //
+//        allOne.inc("b"); //2
+//        allOne.inc("b"); // //3
+//        allOne.inc("b"); // //4
+//        String xa2asd = allOne.getMaxKey(); //
+//        String ia2asdasd = allOne.getMinKey();
+//        allOne.inc("a"); //3
+//        allOne.inc("a"); //4
+//        allOne.inc("a"); //5
+//        allOne.inc("a"); //6
 //        String xb2 = allOne.getMaxKey();
 //        String ia1 = allOne.getMinKey();
-//    }
+//        allOne.inc("c"); //1
+//        allOne.inc("c"); //2
+//        String xb21 = allOne.getMaxKey();
+//        String ia11 = allOne.getMinKey();
+//        allOne.inc("c"); //3
+//        allOne.inc("c"); //4
+//        allOne.inc("c"); //5
+//        allOne.inc("c"); //6
+//        allOne.inc("c"); //7
+//        allOne.inc("c"); //8
+//        String xb21s = allOne.getMaxKey();
+//        String ia11a = allOne.getMinKey();
+//
+//
+//        allOne.dec("d"); //
+//        allOne.dec("d"); //
+//        allOne.dec("d"); //
+//        allOne.inc("d"); //1
+//        String xb2dd1s = allOne.getMaxKey();
+//        String ia11dda = allOne.getMinKey();
+//        allOne.dec("d"); //
+//        allOne.dec("d"); //
+//        String xb2dssd1s = allOne.getMaxKey();
+//        String ia11dssda = allOne.getMinKey();
+//        allOne.dec("c"); //7
+//        allOne.dec("c"); //6
+//        String xb2dssd1scc = allOne.getMaxKey();
+//        String ia11dssdcca = allOne.getMinKey();
+//        allOne.dec("c"); //5
+//        String xb2dssd1ddscc = allOne.getMaxKey();
+//        String ia11dssdddcca = allOne.getMinKey();
+//        allOne.dec("c"); //4
+//        allOne.dec("c"); //3
+//        String xb2dssd1ddsccd = allOne.getMaxKey();
+//        String ia11dssdddccad = allOne.getMinKey();
+//        allOne.dec("c"); //2
+//        allOne.dec("c"); //1
+//        String xb2dssd1ddsccdd = allOne.getMaxKey();
+//        String ia11dssdddccdad = allOne.getMinKey();
+//        allOne.dec("a"); //5
+//        allOne.dec("a"); //4
+//        allOne.dec("a"); //3
+//        String xb2dssd1dsdsccdd = allOne.getMaxKey();
+//        String ia11dssddadccdad = allOne.getMinKey();
+//        allOne.dec("a"); //5
+//        allOne.dec("a"); //4
+//        allOne.dec("a"); //3
+    }
 
-    private Comparator<DataModel> DataModelComparator = (o1, o2) -> {
-        if (o1.key.equals(o2.key))
-            return 0;
-        if (o1.getValue() < o2.getValue())
-            return 1;
-        if (o1.getValue() > o2.getValue())
-            return -1;
-        return -1;
-    };
-
-    private class DataModel {
+    public class DataModel implements Comparable<DataModel> {
         int value;
         String key;
+
+        DataModel smaller;
+        DataModel bigger;
 
         DataModel(String k, int v) {
             value = v;
@@ -64,11 +134,42 @@ public class AllOne {
         public String getKey() {
             return key;
         }
+
+        public boolean noBiggerThan(DataModel dataModel) {
+            return this.value <= dataModel.value;
+        }
+
+        public boolean noSmallerThan(DataModel dataModel) {
+            return this.value >= dataModel.value;
+        }
+
+        @Override
+        public int compareTo(DataModel o) {
+            if (this.key.equals(o.key))
+                return 0;
+            if (this.value <= o.value)
+                return -1;
+            else
+                return 1;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (!(object instanceof DataModel))
+                return false;
+            return ((DataModel) object).key.equals(this.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return this.key.hashCode();
+        }
     }
 
-    private TreeSet<DataModel> treeSet = new TreeSet<>(DataModelComparator);
+    private DataModel smallest;
+    private DataModel biggest;
 
-    private HashMap<String, Integer> relation = new HashMap<>();
+    private HashMap<String, DataModel> data = new HashMap<>();
 
     /**
      * Initialize your data structure here.
@@ -80,18 +181,85 @@ public class AllOne {
      * Inserts a new key <Key> with value 1. Or increments an existing key by 1.
      */
     public void inc(String key) {
-        if (relation.containsKey(key)) {
-            DataModel old = new DataModel(key, relation.get(key));
-            relation.remove(key);
-            treeSet.remove(old);
-            int newValue = old.value + 1;
+        DataModel dataModel = data.get(key);
+        if (dataModel == null) { // new key
+            DataModel newElement = new DataModel(key, 1); // create new instance
 
-            relation.put(key, newValue);
-            DataModel newModel = new DataModel(key, newValue);
-            treeSet.add(newModel);
-        } else {
-            relation.put(key, 1);
-            treeSet.add(new DataModel(key, 1));
+            // start sorting
+            if (smallest == null) { // empty list
+                data.put(key, newElement);
+                smallest = newElement;
+                biggest = newElement;
+            } else { // has element, override smallest; old smallest compare to biggest
+                smallest.smaller = newElement;
+                newElement.bigger = smallest;
+
+                // in case only one element.
+                if (biggest.equals(smallest)) {
+                    biggest.smaller = newElement;
+                    smallest.bigger = biggest;
+                    data.put(biggest.key, biggest);
+                }
+                data.put(smallest.key, smallest);
+                smallest = newElement;
+                data.put(key, newElement);
+            }
+        } else { // old key
+            dataModel.value = dataModel.value + 1;
+
+            boolean isBiggest = dataModel.equals(biggest);
+            boolean isSmallest = dataModel.equals(smallest);
+
+            if (isBiggest && isSmallest) {
+                data.put(key, dataModel);
+                return;
+            } else if (isBiggest) {
+                data.put(key, dataModel);
+                return;
+            }
+
+            if (dataModel.compareTo(dataModel.bigger) > 0) {
+                if (isSmallest) {
+                    dataModel.bigger.smaller = null;
+                    smallest = dataModel.bigger;
+                } else {
+                    dataModel.bigger.smaller = dataModel.smaller;
+                    dataModel.smaller.bigger = dataModel.bigger;
+                }
+
+                // reinsert this element
+                DataModel i = dataModel.bigger;
+                while (true) {
+                    if (i.noSmallerThan(dataModel)) { // found the new index
+
+                        if (i.equals(smallest)) {
+                            smallest.smaller = dataModel;
+                            dataModel.smaller = null;
+                            dataModel.bigger = smallest;
+                            smallest = dataModel;
+                            data.put(key, dataModel);
+                            return;
+                        } else {
+                            dataModel.smaller = i.smaller;
+                            dataModel.bigger = i;
+                            i.smaller.bigger = dataModel;
+                            i.smaller = dataModel;
+                            data.put(key, dataModel);
+                            return;
+                        }
+                    }
+
+                    if (i.equals(biggest)) {
+                        biggest.bigger = dataModel;
+                        dataModel.smaller = biggest;
+                        dataModel.bigger = null;
+                        biggest = dataModel;
+                        data.put(key, dataModel);
+                        return;
+                    }
+                    i = i.bigger;
+                }
+            }
         }
     }
 
@@ -99,15 +267,80 @@ public class AllOne {
      * Decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
      */
     public void dec(String key) {
-        if (relation.containsKey(key)) {
-            DataModel old = new DataModel(key, relation.get(key));
-            relation.remove(key);
-            treeSet.remove(old);
-            int newValue = old.value - 1;
-            if (newValue > 0) {
-                relation.put(key, newValue);
-                treeSet.add(new DataModel(key, newValue));
+        DataModel toDec = data.get(key);
+        if (toDec == null)
+            return;
+
+
+        if (toDec.value == 1) {
+            if (toDec.equals(smallest) && toDec.equals(biggest)) {
+                data.clear();
+                smallest = null;
+                biggest = null;
+                return;
             }
+            if (toDec.equals(smallest)) {
+                toDec.bigger.smaller = null;
+                smallest = toDec.bigger;
+                data.remove(key);
+                return;
+            }
+
+            if (toDec.equals(biggest)) {
+                toDec.smaller.bigger = null;
+                biggest = toDec.smaller;
+                data.remove(key);
+                return;
+            }
+
+            toDec.bigger.smaller = toDec.smaller;
+            toDec.smaller.bigger = toDec.bigger;
+            data.remove(key);
+            return;
+        }
+
+        toDec.value = toDec.value - 1;
+        if (toDec.equals(smallest)) {
+            data.put(key, toDec);
+            return;
+        }
+
+        if (toDec.noSmallerThan(toDec.smaller)) {
+            data.put(key, toDec);
+            return;
+        }
+
+        if (toDec.equals(biggest)) {
+            toDec.smaller.bigger = null;
+            biggest = toDec.smaller;
+        } else {
+            toDec.smaller.bigger = toDec.bigger;
+            toDec.bigger.smaller = toDec.smaller;
+        }
+
+        DataModel i = toDec.smaller;
+        while (true) {
+            if (i.noBiggerThan(toDec)) {
+                i.bigger.smaller = toDec;
+                i.bigger = toDec;
+
+                toDec.bigger = i.bigger.smaller;
+                toDec.smaller = i;
+                data.put(key, toDec);
+
+                return;
+            }
+
+            if (i.equals(smallest)) {
+                smallest.smaller = toDec;
+                toDec.bigger = smallest;
+                toDec.smaller = null;
+                smallest = toDec;
+                data.put(key, toDec);
+                return;
+            }
+
+            i = i.smaller;
         }
     }
 
@@ -115,20 +348,18 @@ public class AllOne {
      * Returns one of the keys with maximal value.
      */
     public String getMaxKey() {
-        if(treeSet.isEmpty())
-            return "";
-        DataModel first = treeSet.first();
-        return first.key;
+        if (biggest != null)
+            return biggest.key;
+        return "";
     }
 
     /**
      * Returns one of the keys with Minimal value.
      */
     public String getMinKey() {
-        if(treeSet.isEmpty())
-            return "";
-        DataModel first = treeSet.last();
-        return first.key;
+        if (smallest != null)
+            return smallest.key;
+        return "";
     }
     /**
      * Your AllOne object will be instantiated and called as such:
