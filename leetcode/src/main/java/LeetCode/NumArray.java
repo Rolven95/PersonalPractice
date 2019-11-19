33324e99847e9b61dae810a7d45fec97dc1cebd3
Ruowen <chengruowen@vip.qq.com>
2019-11-19 21:01:04 +0800

    [LeetCode] NumArray

---
+++ leetcode/src/main/java/LeetCode/NumArray.java
@@ -0,0 +1,54 @@
+package LeetCode;
+
+import javafx.scene.chart.NumberAxis;
+
+/**
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ * <p>
+ * Example:
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ * <p>
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * Note:
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NumArray {
+    /**
+     * Your NumArray object will be instantiated and called as such:
+     * NumArray obj = new NumArray(nums);
+     * int param_1 = obj.sumRange(i,j);
+     */
+
+    private int[] sums;
+
+    public NumArray(int[] nums) {
+        sums = new int[nums.length];
+        if (nums.length == 0) {
+            return;
+        }
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] += sums[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0) {
+            return sums[j];
+        } else {
+            return sums[j] - sums[i - 1];
+        }
+    }
+
+    public static void main(String[] args) {
+        NumArray numArray = new NumArray(new int[]{-2, 0, 3, -5, 2, -1});
+        numArray.sumRange(1, 2);
+    }
+}
